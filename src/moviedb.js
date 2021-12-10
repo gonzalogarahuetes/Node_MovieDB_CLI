d1b01fc6497a1156006af71747dd9c855d7e674b
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-12-10 14:55:41 +0100

    1.1.5.0  requirements

--- src/moviedb.js
+++ src/moviedb.js
@@ -5,19 +5,21 @@ const { Command } = require("commander");
 const program = new Command();
 program.version("0.0.1");
 
+// Commands
+
 program
   .command("get-persons")
   .description("Make a network request to fetch most popular persons")
   .action(function handleAction() {
-    console.log("hello-world");
+    const options = program.opts();
+
+    if (options.page) console.log(options.page);
   });
 
 program
   .command("get-person")
   .description("Make a network request to fetch the data of a single person")
-  .action(function handleAction() {
-    console.log("hello-world");
-  });
+  .action(function handleAction() {});
 
 program
   .command("get-movies")
@@ -33,6 +35,25 @@ program
     console.log("hello-world");
   });
 
+// Options
+
+program
+  .requiredOption("-p, --popular", "Fetch the popular persons")
+  .requiredOption(
+    "--page <number>",
+    "The page of persons data results to fetch",
+    myParseInt
+  );
+
 // error on unknown commands
+
+function myParseInt(value, dummyPrevious) {
+  // parseInt takes a string and a radix
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new commander.InvalidArgumentError("Not a number.");
+  }
+  return parsedValue;
+}
 
 program.parse(process.argv);
